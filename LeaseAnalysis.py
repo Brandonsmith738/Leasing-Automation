8ac94e170afecb7e602cfca079db75fd26f784c6
Brandonsmith738 <boombyeyt@gmail.com>
2022-07-22 10:36:46 -0500

    init

---
+++ LeaseAnalysis.py
@@ -0,0 +1,38 @@
+import xlwings as xw
+import TenantData
+from TenantData import Tenant
+from TenantData import NewTenant
+
+def createTemplate(PropertyIndex):
+    # Assign Space Square Feet
+    # Initialize Template with xw
+    # wb = xw.Book('Lease_Analysis_Template.xlsx')
+    wb = xw.Book(r'C:\Users\Boomb\PycharmProjects\LeasingAutomationGui\Lease_Analysis_Template.xlsx')
+    sheet = wb.sheets['Analysis']
+    data = wb.sheets['Data Grid']
+    proposal = wb.sheets['Proposal']
+    sheet.range('C7').value = NewTenant.getTenant()
+    sheet.range('C10').value = NewTenant.getLeaseTerm()
+    sheet.range('C9').value = data.range('F' + str(PropertyIndex + 7)).value
+    sheet.range('C5').value = data.range('A' + str(PropertyIndex + 7)).value
+    # Push each value from base rent list to Lease Analysis sheet
+    for i in range(int(len(NewTenant.getRentBase()))):
+        sheet.range('C'+str(i+20)).value = NewTenant.getRentBaseElement(i)
+        # Append the rest of the table to their corresponding lists
+        NewTenant.appendMonthlyBase(proposal.range('D'+str(i+18)).value)
+        NewTenant.appendMonthlyImprovement(proposal.range('E'+str(i+18)).value)
+        NewTenant.appendEstAdditionalRent(proposal.range('F'+str(i+18)).value)
+        NewTenant.appendTotalMonthly(proposal.range('G'+str(i+18)).value)
+        NewTenant.appendTotalYearly(proposal.range('I'+str(i+18)).value)
+    # Pull total rent
+    NewTenant.setTotalRent(proposal.range('I29').value)
+    # Setting the property from drop down list
+    NewTenant.setPropertySF(sheet.range('C9').value)
+    NewTenant.setProperty(data.range('A' + str(PropertyIndex + 7)).value)
+    NewTenant.setTrippleNets(data.range('U' + str(PropertyIndex + 7)).value)
+
+    NewTenant.setLandlordTI(sheet.range('C12').value)
+    NewTenant.setSecurityDepost(proposal.range('O18').value)
+
+    wb.save(NewTenant.getTenant() + "/" + NewTenant.getTenant() + 'LA.xlsx')
+    wb.close()
---
+++ TenantData.py
@@ -0,0 +1,158 @@
+import os, sys
+from datetime import date
+
+
+# Pull the current date
+today = date.today()
+def getTodayMonth():
+    return int(today.strftime("%m"))
+def getTodayYear():
+    return int(today.strftime("%Y"))
+def getToday():
+    return int(today.strftime("%d"))
+
+# Format the date of writing LOI
+todays_date = today.strftime("%B %d, %Y")
+
+# Lease Analysis Variables
+class Tenant():
+    def __init__(self):
+        self._Property = 0
+        self._PropertySF = 0
+        self._FirstName = 0
+        self._Tenant = 0
+        self._LeaseTerm = 0
+        self._RentAbatement = 0
+        self._CommissionRate = 0
+        self._AnnualRent = 0
+        self._SpaceAddress = 0
+        self._SpaceLocation = 0
+        self._PossessionDate = 0
+        self._CommencementDays = 0
+        self._DeltaDate = 0
+        self._TrippleNets = 0
+        self._TenantWork = ""
+        self._LandlordWork = ""
+        self._ExtraConstingency = ""
+        self._LandlordTI = 0
+        self._SecurityDeposit = 0
+        self._TenantFinishCheckbox = False
+        self._LandlordWorkExist = False
+
+    #     Tenant Rent Table
+        self._rent_sqft = []
+        self._monthly_base = []
+        self._monthly_improvement = []
+        self._est_additional_rent = []
+        self._total_monthly = []
+        self._total_yearly = []
+        self._total_rent = 0
+
+    def getRentBase(self):
+        return self._rent_sqft
+
+    def setTotalRent(self, x):
+        self._total_rent = x
+    def getTotalRent(self):
+        return self._total_rent
+
+    def getRentBaseElement(self, x):
+        return self._rent_sqft[x]
+    def getMonthlyBaseElement(self, x):
+        return self._monthly_base[x]
+    def getMonthlyImprovementElement(self, x):
+        return self._monthly_improvement[x]
+    def getEstAdditionalRentElement(self, x):
+        return self._est_additional_rent[x]
+    def getTotalMonthlyElement(self, x):
+        return self._total_monthly[x]
+    def getTotalYearlyElement(self, x):
+        return self._total_yearly[x]
+    # Rent Table Append
+    def appendRentSqft(self, x):
+        self._rent_sqft.append(x)
+    def appendMonthlyBase(self, x):
+        self._monthly_base.append(x)
+    def appendMonthlyImprovement(self, x):
+        self._monthly_improvement.append(x)
+    def appendEstAdditionalRent(self, x):
+        self._est_additional_rent.append(x)
+    def appendTotalMonthly(self, x):
+        self._total_monthly.append(x)
+    def appendTotalYearly(self, x):
+        self._total_yearly.append(x)
+
+    def setTenant(self, x):
+        self._Tenant = x
+    def getTenant(self):
+        return self._Tenant
+    def setPropertySF(self, x):
+        self._PropertySF = x
+    def getPropertySF(self):
+        return self._PropertySF
+    def setFirstName(self, x):
+        self._FirstName = x
+    def getFirstName(self):
+        return self._FirstName
+    def setSpaceAddress(self, x):
+        self._SpaceAddress = x
+    def getSpaceAddress(self):
+        return self._SpaceAddress
+    def setProperty(self, x):
+        self._Property = x
+    def getProperty(self):
+        return self._Property
+    def setLeaseTerm(self, x):
+        self._LeaseTerm = x
+    def getLeaseTerm(self):
+        return self._LeaseTerm
+    def setSpaceLocation(self, x):
+        self._SpaceLocation = x
+    def getSpaceLocation(self):
+        return self._SpaceLocation
+    def setPossessionDate(self, x):
+        self._PossessionDate = x
+    def getPossessionDate(self):
+        return self._PossessionDate
+    def setCommencementDays(self, x):
+        self._CommencementDays = x
+    def getCommencementDays(self):
+        return self._CommencementDays
+    def setDeltaDate(self, x):
+        self._DeltaDate = x
+    def getDeltaDate(self):
+        return self._DeltaDate
+    def setTrippleNets(self, x):
+        self._TrippleNets = x
+    def getTrippleNets(self):
+        return self._TrippleNets
+    def setTenantWork(self, x):
+        self._TenantWork = x
+    def getTenantWork(self):
+        return self._TenantWork
+    def setLandlordWork(self, x):
+        self._LandlordWork = x
+    def getLandlordWork(self):
+        return self._LandlordWork
+    def setExtraConstingency(self, x):
+        self._ExtraConstingency = x
+    def getExtraConstingency(self):
+        return self._ExtraConstingency
+    def setLandlordTI(self, x):
+        self._LandlordTI = x
+    def getLandlordTI(self):
+        return self._LandlordTI
+    def getSecurityDeposit(self):
+        return self._SecurityDeposit
+    def setSecurityDepost(self, x):
+        self._SecurityDeposit = x
+    def setTenantFinishCheckbox(self, x):
+        self._TenantFinishCheckbox = x
+    def getTenantFinishCheckbox(self):
+        return self._TenantFinishCheckbox
+    def setLandlordWorkExist(self, x):
+        self._LandlordWorkExist = x
+    def getLandlordWorkExist(self):
+        return self._LandlordWorkExist
+
+NewTenant = Tenant()
