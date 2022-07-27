86a4bd724ed59c852f65914375ac84360adecf73
Chris P. Cream <ryflores15@gmail.com>
2022-07-26 22:52:48 -0500

    RyanUpdate

--- main.py
+++ main.py
@@ -61,82 +61,67 @@ class Window(Frame):
         PropertiesDropdown.set('21 Webb')  # set the default option
 
         # Row 1
-        TenantLabel = Label(root, text="Tenant Business Name")
+        TenantLabel = Label(root, text="Tenant Business Name", font=('Times', 12))
         TenantLabel.pack(side=LEFT)
-        TenantLabel.place(relx=0, rely=0)
+        TenantLabel.place(relx=.05, rely=.01)
         TenantEntry = Entry(root, bd=5)
         TenantEntry.pack(side=RIGHT)
-        TenantEntry.place(relx=0.11, rely=0)
+        TenantEntry.place(relx=.12, rely=.01)
 
-        LeadFirstNameLabel = Label(root, text="Lead First Name")
+        LeadFirstNameLabel = Label(root, text="Lead First Name", font=('Times', 12))
         LeadFirstNameLabel.pack(side=LEFT)
-        LeadFirstNameLabel.place(relx=0.25, rely=0)
+        LeadFirstNameLabel.place(relx=0.24, rely=.01)
         LeadFirstNameEntry = Entry(root, bd=5)
         LeadFirstNameEntry.pack(side=RIGHT)
-        LeadFirstNameEntry.place(relx=0.33, rely=0)
+        LeadFirstNameEntry.place(relx=0.29, rely=.01)
 
-        SpaceLocationLabel = Label(root, text="Space Address")
+        SpaceLocationLabel = Label(root, text="Space Address", font=('Times', 12))
         SpaceLocationLabel.pack(side=LEFT)
-        SpaceLocationLabel.place(relx=0.45, rely=0)
+        SpaceLocationLabel.place(relx=0.39, rely=.01)
         SpaceLocationEntry = Entry(root, bd=5)
         SpaceLocationEntry.pack(side=RIGHT)
-        SpaceLocationEntry.place(relx=0.52, rely=0)
+        SpaceLocationEntry.place(relx=0.44, rely=.01)
 
         drop = OptionMenu(root, PropertiesDropdown, *choices)
         drop.pack()
-        drop.place(relx=0.63, rely=0)
+        drop.place(relx=0.5, rely=.01)
         print(choices.index(PropertiesDropdown.get()))
 
-        def pushToBaseRentList():
-            # Append each entry onto the NewTenant Obj
-            NewTenant.appendRentSqft(BaseRentEntry.get())
-            # For development
-            print(NewTenant.getRentBase())
-            # Here we loop through the list of base rent, we are displaying it onto the GUI
-            # This will actually be DELETED. NEW PROJECT: Giant Linear Algebra problem that gives the below as possible solutions
-            # Problem,,,,, How to show the possible solutions with Tkinter
-            for i in range(int(len(NewTenant.getRentBase()))):
-                ShowBaseRentList = Label(root, text='Year '+str(i+1)+'      '+str(NewTenant.getRentBaseElement(i)))
-                ShowBaseRentList.pack(side=LEFT)
-                ShowBaseRentList.place(relx=0.85, rely=0.1+(i/40))
-
-        BaseRentLabel = Label(root, text="Yearly Base Rent")
+        BaseRentLabel = Label(root, text="Yearly Base Rent", font=('Times', 12))
         BaseRentLabel.pack(side=LEFT)
-        BaseRentLabel.place(relx=0.76, rely=0)
+        BaseRentLabel.place(relx=0.58, rely=.01)
         BaseRentEntry = Entry(root, bd=5)
         BaseRentEntry.pack(side=RIGHT)
-        BaseRentEntry.place(relx=0.85, rely=0)
+        BaseRentEntry.place(relx=0.63, rely=.01)
 
-        BaseRentButton = Button(self, text='Add Base Rent', command=pushToBaseRentList, bg='orange', fg='white')
-        BaseRentButton.place(x=1100, y=30)
         # Row 2
-        TermLengthLabel = Label(root, text="Term Length (yrs)")
+        TermLengthLabel = Label(root, text="Term Length (yrs)", font=('Times', 12))
         TermLengthLabel.pack(side=LEFT)
-        TermLengthLabel.place(relx=0, rely=0.1)
+        TermLengthLabel.place(relx=.73, rely=.01)
         TermLengthEntry = Entry(root, bd=5)
         TermLengthEntry.pack(side=RIGHT)
-        TermLengthEntry.place(relx=0.11, rely=0.1)
+        TermLengthEntry.place(relx=0.78, rely=.01)
 
-        LandlordWorkLabel = Label(root, text="Landlord's Work: ")
+        LandlordWorkLabel = Label(root, text="Landlord's Work: ", font=('Times', 12))
         LandlordWorkLabel.pack(side=LEFT)
-        LandlordWorkLabel.place(relx=0, rely=0.15)
+        LandlordWorkLabel.place(relx=.05, rely=0.15)
         LandlordWorkEntry = Text(root, height=5, width=52)
         LandlordWorkEntry.pack(side=RIGHT)
         LandlordWorkEntry.place(relx=0.1, rely=0.15)
 
-        TenantWorkLabel = Label(root, text="Tenant's Work: ")
+        TenantWorkLabel = Label(root, text="Tenant's Work: ", font=('Times', 12))
         TenantWorkLabel.pack(side=LEFT)
-        TenantWorkLabel.place(relx=0, rely=0.3)
+        TenantWorkLabel.place(relx=.05, rely=0.3)
         TenantWorkEntry = Text(root, height=5, width=52)
         TenantWorkEntry.pack(side=RIGHT)
         TenantWorkEntry.place(relx=0.1, rely=0.3)
 
-        ExtraContingencyLabel = Label(root, text="Extra Contingencies: ")
+        ExtraContingencyLabel = Label(root, text="Extra Contingencies: ", font=('Times', 12))
         ExtraContingencyLabel.pack(side=LEFT)
-        ExtraContingencyLabel.place(relx=0, rely=0.6)
+        ExtraContingencyLabel.place(relx=.40, rely=0.15)
         ExtraContingencyEntry = Text(root, height=5, width=52)
         ExtraContingencyEntry.pack(side=RIGHT)
-        ExtraContingencyEntry.place(relx=0.1, rely=0.6)
+        ExtraContingencyEntry.place(relx=0.46, rely=0.15)
         # Check button for Tenant finish ?
         Checkbutton1 = BooleanVar()
 
@@ -144,16 +129,16 @@ class Window(Frame):
                               variable=Checkbutton1,
                               onvalue=1,
                               offvalue=0,
-                              height=2,
-                              width=10)
-        TenantFinishCheckbox.place(relx=0.082, rely=0.45)
+                              height=10,
+                              width=20, font=('Times', 12))
+        TenantFinishCheckbox.place(relx=.47, rely=0.25)
 
         # Calendar for Commencement Days
         cal = Calendar(root, selectmode='day',
                        year=TenantData.getTodayYear(), month=TenantData.getTodayMonth(),
                        day=TenantData.getToday())
-        cal.pack(pady=20)
-        cal.place(relx=0.76, rely=0.5)
+        cal.pack(padx=20)
+        cal.place(relx=0.75, rely=.18)
 
         # Calculates the difference from today to commencement
         def getDeltaDate():
@@ -196,8 +181,10 @@ class Window(Frame):
             LeaseAnalysis.createTemplate(choices.index(PropertiesDropdown.get()))
             PopulateLOI.createTemplate()
 
-        ExecuteButton = Button(self, text='Calculate', command=getData, width=20, height=5, bg='blue', fg='white')
-        ExecuteButton.place(x=1055,y=550)
+        CalculateButton = Button(self, command=getData, width=400, height=30, bg='orange')
+        CalculateButton.place(relx=0, rely=.75)
+        CalculateLabel = Label(root, text = 'Execute Automation', font=('Times, 100'), fg='white', bg='orange')
+        CalculateLabel.place(relx=.275, rely=.8)
 
     def client_exit(self):
         exit()
