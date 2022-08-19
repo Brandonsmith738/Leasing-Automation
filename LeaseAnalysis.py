e9cd837b64a0e69f951fb9fe1c55b6ccc29e55d7
Chris P. Cream <108816114+RFLORES150@users.noreply.github.com>
2022-08-19 11:15:17 -0500

    Update LeaseAnalysis.py

--- LeaseAnalysis.py
+++ LeaseAnalysis.py
@@ -17,15 +17,7 @@ def createTemplate(PropertyIndex):
         sheet.range('C10').value = NewTenant.getLeaseTerm()
         sheet.range('C9').value = data.range('F' + str(PropertyIndex + 7)).value
         sheet.range('C5').value = data.range('A' + str(PropertyIndex + 7)).value
-        # Push each value from base rent list to Lease Analysis sheet
-        for i in range(int(len(NewTenant.getRentBase()))):
-            sheet.range('C'+str(i+20)).value = NewTenant.getRentBaseElement(i)
-            # Append the rest of the table to their corresponding lists
-            NewTenant.appendMonthlyBase(proposal.range('D'+str(i+18)).value)
-            NewTenant.appendMonthlyImprovement(proposal.range('E'+str(i+18)).value)
-            NewTenant.appendEstAdditionalRent(proposal.range('F'+str(i+18)).value)
-            NewTenant.appendTotalMonthly(proposal.range('G'+str(i+18)).value)
-            NewTenant.appendTotalYearly(proposal.range('I'+str(i+18)).value)
+        # Test
         # Pull total rent
         NewTenant.setTotalRent(proposal.range('I29').value)
         # Setting the property from drop down list
@@ -36,7 +28,7 @@ def createTemplate(PropertyIndex):
         NewTenant.setLandlordTI(sheet.range('C12').value)
         NewTenant.setSecurityDepost(proposal.range('O18').value)
         
-            n = float(NewTenant.getLeaseTerm())
+    n = float(NewTenant.getLeaseTerm())
     step = 0.5
     possibleValues = np.arange(0, 201)*0.25
 
