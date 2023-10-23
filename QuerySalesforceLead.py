7f4e384c181f20cc60bad87d73e1feeb0255eb8f
Brandon Smith <88470211+Brandonsmith738@users.noreply.github.com>
2023-10-23 15:01:56 -0500

    Update QuerySalesforceLead.py

--- QuerySalesforceLead.py
+++ QuerySalesforceLead.py
@@ -2,4 +2,4 @@ import simple_salesforce
 from simple_salesforce import Salesforce
 
 # Need to grab token from Calvin
-sf = Salesforce(username='bob.ale-ebrahim@occmgmt.com', password='t4TX&5QT', security_token='qfn6mm7eDizLwSLlOecAZ8PNN')
+sf = Salesforce(username='', password='', security_token='')
